e6d21915c47130574a2425411bfdb4b4a7818eeb
MRadjaNurAkbarA25 <gulalimerahjambu16@gmail.com>
2025-10-23 06:49:14 +0700

    kiki

---
+++ kelas/pertemuan-7/fungsi dan prosedur.py
@@ -0,0 +1,23 @@
+# def perkenalan():
+#     print('Mata kuliah')
+#     print('Kalkulus')
+#     input('Tekan enter untuk lanjut....')
+    
+# perkenalan()
+
+# def perkalian ():
+#     x = 5*5
+#     print(x)    
+    
+# perkalian()
+
+# def perkenalan(nama):
+#     print(f'Halo {nama} selamat berbelanja')
+    
+# perkenalan('Asep')
+
+def luasPersegiPanjang(panjang, lebar):
+    luas = panjang * lebar
+    print(f'Luas dari persegi panjang adalah {luas}')
+    
+luasPersegiPanjang(4, 2)
